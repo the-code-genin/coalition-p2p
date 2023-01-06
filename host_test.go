b4419728915835062c261c2c9a557eb7aa314027
mohammed.adekunle <adekunle3317@gmail.com>
2023-01-06 13:27:13 +0100

    Updated new host tests

--- host_test.go
+++ host_test.go
@@ -1,22 +1,30 @@
 package framework
 
 import (
+	"bytes"
 	"crypto/ed25519"
 	"crypto/rand"
+	"crypto/sha256"
 	"testing"
 )
 
 func TestNewHost(t *testing.T) {
 	// Generate a key pair
-	_, key, err := ed25519.GenerateKey(rand.Reader)
+	pubKey, privKey, err := ed25519.GenerateKey(rand.Reader)
 	if err != nil {
 		t.Error(err)
 	}
 
 	// Create a new host
-	host, err := NewHost(3000, key)
+	host, err := NewHost(3000, privKey)
 	if err != nil {
 		t.Error(err)
+	}
+
+	hostID := host.PeerID()
+	expectedID := sha256.Sum256(pubKey)
+	if !bytes.Equal(expectedID[:], hostID[:]) {
+		t.Errorf("unexpected host ID")
 	}
 
 	if err = host.Close(); err != nil {
