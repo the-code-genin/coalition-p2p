e8211ffac12219697c25ba6de86b93ea37f4b008
mohammed.adekunle <adekunle3317@gmail.com>
2023-01-06 13:22:01 +0100

    Added addressing data to hosts

--- host.go
+++ host.go
@@ -1,12 +1,40 @@
 package framework
 
 import (
+	"crypto/ed25519"
+	"crypto/sha256"
 	"fmt"
 	"net"
+	"regexp"
+	"strconv"
 )
 
 type Host struct {
 	listener net.Listener
+	key      ed25519.PrivateKey
+}
+
+// Return the listening address
+func (host *Host) Address() string {
+	return host.listener.Addr().String()
+}
+
+func (host *Host) Port() (int, error) {
+	regExp := regexp.MustCompile(`^.+\:(\d+)$`)
+	match := regExp.FindAllStringSubmatch(host.Address(), -1)
+	if len(match) != 1 {
+		return 0, fmt.Errorf("unable to parse host port")
+	} else if len(match[0]) != 2 {
+		return 0, fmt.Errorf("unable to parse host port")
+	}
+
+	return strconv.Atoi(match[0][1])
+}
+
+// Returns the 256-bit hash of the public key as the peer ID
+func (host *Host) PeerID() [32]byte {
+	pk := host.key.Public().(ed25519.PublicKey)
+	return sha256.Sum256([]byte(pk))
 }
 
 // Close the host and any associated resources
@@ -14,9 +42,12 @@ func (host *Host) Close() error {
 	return host.listener.Close()
 }
 
-// Create a new P2P host on the specified host
-func NewHost(port int) (*Host, error) {
-	// Open listening port at the port
+// Create a new P2P host on the specified port with the Ed25519 private key
+func NewHost(
+	port int,
+	key ed25519.PrivateKey,
+) (*Host, error) {
+	// Start listening on the port
 	listener, err := net.Listen("tcp", fmt.Sprintf("0.0.0.0:%d", port))
 	if err != nil {
 		return nil, err
@@ -25,6 +56,7 @@ func NewHost(port int) (*Host, error) {
 	// Create a new host
 	host := Host{
 		listener,
+		key,
 	}
 
 	return &host, nil
--- host_test.go
+++ host_test.go
@@ -1,13 +1,24 @@
 package framework
 
-import "testing"
+import (
+	"crypto/ed25519"
+	"crypto/rand"
+	"testing"
+)
 
 func TestNewHost(t *testing.T) {
-	host, err := NewHost(300)
+	// Generate a key pair
+	_, key, err := ed25519.GenerateKey(rand.Reader)
 	if err != nil {
 		t.Error(err)
 	}
-	
+
+	// Create a new host
+	host, err := NewHost(3000, key)
+	if err != nil {
+		t.Error(err)
+	}
+
 	if err = host.Close(); err != nil {
 		t.Error(err)
 	}
