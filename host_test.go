f485a5c502cc26183f48420c70cdf15161ca45da
mohammed.adekunle <adekunle3317@gmail.com>
2023-01-06 13:29:01 +0100

    Added port test to hosts

--- host_test.go
+++ host_test.go
@@ -16,15 +16,25 @@ func TestNewHost(t *testing.T) {
 	}
 
 	// Create a new host
-	host, err := NewHost(3000, privKey)
+	port := 3000
+	host, err := NewHost(port, privKey)
 	if err != nil {
 		t.Error(err)
 	}
 
+	// Ensure host has correct peer ID
 	hostID := host.PeerID()
 	expectedID := sha256.Sum256(pubKey)
 	if !bytes.Equal(expectedID[:], hostID[:]) {
 		t.Errorf("unexpected host ID")
+	}
+
+	// Ensure host is listening on the right port
+	hostPort, err := host.Port()
+	if err != nil {
+		t.Error(err)
+	} else if hostPort != port {
+		t.Errorf("unexpected host port")
 	}
 
 	if err = host.Close(); err != nil {
