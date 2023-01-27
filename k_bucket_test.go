2a7bb1da48b2c76fee2433d692b4e63cd6bb22f3
mohammed.adekunle <adekunle3317@gmail.com>
2023-01-27 01:39:05 +0100

    Added test for expiried entries

--- k_bucket_test.go
+++ k_bucket_test.go
@@ -26,3 +26,24 @@ func TestKBucketStorage(t *testing.T) {
 		}
 	}
 }
+
+func TestKBucketStorageExpiry(t *testing.T) {
+	replicationParam := int64(20)
+	pingPeriod := int64(-1)
+	bucket, err := NewKBucket(replicationParam, pingPeriod)
+	if err != nil {
+		t.Error(err)
+	}
+
+	// Add three times the number of replication entries
+	for i := int64(0); i < replicationParam*3; i++ {
+		key := make([]byte, PeerIDSize)
+		if _, err := rand.Read(key); err != nil {
+			t.Error(err)
+		}
+
+		if !bucket.Insert(key, i) {
+			t.Errorf("A new entry should be inserted")
+		}
+	}
+}
