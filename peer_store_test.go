6a0f417720463cee0ad2783785080736e46c6735
mohammed.adekunle <adekunle3317@gmail.com>
2023-01-28 13:55:55 +0100

    Implemented deletion test for peer store

--- peer_store_test.go
+++ peer_store_test.go
@@ -1,7 +1,7 @@
 package coalition
 
 import (
-	// "bytes"
+	"bytes"
 	"crypto/rand"
 	"testing"
 	"time"
@@ -69,35 +69,39 @@ func TestPeerStoreExpiry(t *testing.T) {
 	}
 }
 
-// func TestPeerStoreRemove(t *testing.T) {
-// 	locusKey := make([]byte, PeerKeySize)
-// 	if _, err := rand.Read(locusKey); err != nil {
-// 		t.Error(err)
-// 	}
-// 	maxPeers := int64(20)
-// 	pingPeriod := int64(time.Hour.Seconds())
-// 	store, err := NewPeerStore(locusKey, maxPeers, pingPeriod)
-// 	if err != nil {
-// 		t.Error(err)
-// 	}
+func TestPeerStoreRemove(t *testing.T) {
+	locusKey := make([]byte, PeerKeySize)
+	if _, err := rand.Read(locusKey); err != nil {
+		t.Error(err)
+	}
+	maxPeers := int64(20)
+	pingPeriod := int64(time.Hour.Seconds())
+	store, err := NewPeerStore(locusKey, maxPeers, pingPeriod)
+	if err != nil {
+		t.Error(err)
+	}
 
-// 	// Populate the bucket
-// 	for i := int64(0); i < maxPeers; i++ {
-// 		key := make([]byte, PeerKeySize)
-// 		if _, err := rand.Read(key); err != nil {
-// 			t.Error(err)
-// 		}
+	// Populate the bucket
+	for i := int64(0); i < maxPeers; i++ {
+		key := make([]byte, PeerKeySize)
+		if _, err := rand.Read(key); err != nil {
+			t.Error(err)
+		}
 
-// 		if !store.Insert(key, "0.0.0.0", int(i)) {
-// 			t.Errorf("A new peer should be inserted")
-// 		}
-// 	}
+		inserted, err := store.Insert(key, "0.0.0.0", int(i))
+		if err != nil {
+			t.Error(err)
+		}
+		if !inserted {
+			t.Errorf("A new peer should be inserted")
+		}
+	}
 
-// 	peers := store.Peers()
-// 	store.Remove(peers[0].key)
-// 	if len(store.Peers()) != len(peers)-1 {
-// 		t.Errorf("bucket should have %d entries", len(peers)-1)
-// 	} else if bytes.Equal(store.Peers()[0].key, peers[0].key) {
-// 		t.Errorf("bucket entry not deleted")
-// 	}
-// }
+	peers := store.Peers()
+	store.Remove(peers[0].key)
+	if len(store.Peers()) != len(peers)-1 {
+		t.Errorf("bucket should have %d entries", len(peers)-1)
+	} else if bytes.Equal(store.Peers()[0].key, peers[0].key) {
+		t.Errorf("bucket entry not deleted")
+	}
+}
