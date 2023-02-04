dc51f59f1f567f00255151c9c77353c142200518
mohammed.adekunle <adekunle3317@gmail.com>
2023-02-04 13:50:29 +0100

    Implemented rpc method to find nodes closest to a key

--- rpc_methods.go
+++ rpc_methods.go
@@ -1,6 +1,9 @@
 package coalition
 
-import "fmt"
+import (
+	"encoding/hex"
+	"fmt"
+)
 
 // Send a ping to the host at the address
 func (host *Host) Ping(address string) error {
@@ -15,4 +18,31 @@ func (host *Host) Ping(address string) error {
 		return fmt.Errorf("expected [%s] as response", PingResponse)
 	}
 	return nil
+}
+
+// Asks a peer for a list of nodes closest to a key on the network
+func (host *Host) FindNode(address string, key []byte) ([]string, error) {
+	response, err := host.SendMessage(
+		address,
+		1,
+		FindNodeMethod,
+		hex.EncodeToString(key),
+	)
+	if err != nil {
+		return nil, err
+	}
+
+	data, ok := response.([]interface{})
+	if !ok {
+		return nil, fmt.Errorf("expected an array of node addresses as response")
+	}
+	addrs := make([]string, 0)
+	for _, raw := range data {
+		addr, ok := raw.(string)
+		if !ok {
+			return nil, fmt.Errorf("expected a string")
+		}
+		addrs = append(addrs, addr)
+	}
+	return addrs, nil
 }
