7f775f0f8a41f1c6898c7435ffb8f1889c887540
mohammed.adekunle <adekunle3317@gmail.com>
2023-02-04 12:20:38 +0100

    Restructured boot nodes

--- examples/boot_nodes/main.go
+++ examples/boot_nodes/main.go
@@ -10,41 +10,32 @@ import (
 var wg sync.WaitGroup
 
 func main() {
-	hostA, err := coalition.NewHost(3000)
-	if err != nil {
-		panic(err)
+	basePort := 3000
+	hosts := make([]*coalition.Host, 0)
+	for i := 0; i < 30; i++ {
+		host, err := coalition.NewHost(basePort + i)
+		if err != nil {
+			panic(err)
+		}
+		defer host.Close()
+		go host.Listen()
+
+		addrs, err := host.Addresses()
+		if err != nil {
+			panic(err)
+		}
+		fmt.Printf("Node listening on [%s]\n", addrs[0])
+
+		// Connect previous host to this host
+		if len(hosts) > 0 {
+			prevHost := hosts[len(hosts)-1]
+			if err := prevHost.Ping(addrs[0]); err != nil {
+				panic(err)
+			}
+		}
+
+		hosts = append(hosts, host)
 	}
-	defer hostA.Close()
-
-	hostB, err := coalition.NewHost(3001)
-	if err != nil {
-		panic(err)
-	}
-	defer hostB.Close()
-
-	addrsA, err := hostA.Addresses()
-	if err != nil {
-		panic(err)
-	}
-	addrsB, err := hostB.Addresses()
-	if err != nil {
-		panic(err)
-	}
-
-	go hostA.Listen()
-	go hostB.Listen()
-
-	fmt.Printf("NodeA listening on [%s]\n", addrsA[0])
-	fmt.Printf("NodeB listening on [%s]\n", addrsB[0])
-
-	// Connect hostB to hostA
-	err = hostB.Ping(addrsA[0])
-	if err != nil {
-		panic(err)
-	}
-
-	fmt.Printf("NodeA has [%d] peers\n", len(hostA.RouteTable().Peers()))
-	fmt.Printf("NodeB has [%d] peers\n", len(hostB.RouteTable().Peers()))
 
 	fmt.Println("Boot nodes online")
 	wg.Add(1)
